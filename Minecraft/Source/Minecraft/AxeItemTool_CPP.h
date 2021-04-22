d3180da27dbd070f4034ad38329e1043306af145
Diego Castellanos <57270722+Xavota@users.noreply.github.com>
2021-04-21 20:53:12 -0500

    Axe

---
+++ Minecraft/Source/Minecraft/AxeItemTool_CPP.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseItemTool_CPP.h"
+#include "AxeItemTool_CPP.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MINECRAFT_API AAxeItemTool_CPP : public ABaseItemTool_CPP
+{
+	GENERATED_BODY()
+	
+public:
+	AAxeItemTool_CPP();
+};
